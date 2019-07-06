c7dcc398fb330a27891a2b2d5d99ce08446088f1
Christian Bedwell <Christian.8edwell@gmail.com>
2019-07-05 23:28:35 -0400

    Revamped UI for authentication page.

--- app/src/main/java/com/example/spotifyauthentication/MainActivity.java
+++ app/src/main/java/com/example/spotifyauthentication/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
@@ -29,19 +32,41 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String CLIENT_ID = "clientid";
     public static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
-    public static final int AUTH_CODE_REQUEST_CODE = 0x11;
 
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken;
-    private String mAccessCode;
     private Call mCall;
+
+    CheckBox checkbox;
+    private Button button_authenticate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle(String.format(
-                Locale.US, "Spotify Auth Sample %s", com.spotify.sdk.android.authentication.BuildConfig.VERSION_NAME));
+                Locale.US, "Spotify Analytics", com.spotify.sdk.android.authentication.BuildConfig.VERSION_NAME));
+
+        // instantiate agreement checkbox and authentication button
+        checkbox = (CheckBox) findViewById(R.id.checkbox);
+        button_authenticate = (Button) findViewById(R.id.authentication_button);
+
+        // by default, checkbox state is off because the user hasn't clicked the checkbox
+        button_authenticate.setEnabled(false);
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            // checks checkbox state, used to toggle authentication button state on and off
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // if user has agreed to authentication statement, enable authentication button
+                if (isChecked) {
+                    button_authenticate.setEnabled(true);
+                }
+                // if user has not agreed to authentication statement, disable authentication button
+                else{
+                    button_authenticate.setEnabled(false);
+                }
+            }
+        });
     }
 
     @Override
@@ -50,47 +75,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public void onGetUserProfileClicked(View view) {
-        if (mAccessToken == null) {
-            final Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_main), R.string.warning_need_token, Snackbar.LENGTH_SHORT);
-            snackbar.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
-            snackbar.show();
-            return;
-        }
-
-        final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me")
-                .addHeader("Authorization","Bearer " + mAccessToken)
-                .build();
-
-        cancelCall();
-        mCall = mOkHttpClient.newCall(request);
-
-        mCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                setResponse("Failed to fetch data: " + e);
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    final JSONObject jsonObject = new JSONObject(response.body().string());
-                    setResponse(jsonObject.toString(3));
-                }
-                catch (JSONException e) {
-                    setResponse("Failed to parse data: " + e);
-                }
-            }
-        });
-    }
-
-    public void onRequestCodeClicked(View view) {
-        final AuthenticationRequest request = getAuthenticationRequest(AuthenticationResponse.Type.CODE);
-        AuthenticationClient.openLoginActivity(this, AUTH_CODE_REQUEST_CODE, request);
-    }
-
-    public void onRequestTokenClicked(View view) {
+    public void requestTokenRequested() {
         final AuthenticationRequest request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN);
         AuthenticationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request);
     }
@@ -112,10 +97,6 @@ public class MainActivity extends AppCompatActivity {
             mAccessToken = response.getAccessToken();
             updateTokenView();
         }
-        else if (AUTH_CODE_REQUEST_CODE == requestCode) {
-            mAccessCode = response.getCode();
-            updateCodeView();
-        }
     }
 
     private void setResponse(final String text) {
@@ -129,13 +110,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateTokenView() {
-        final TextView tokenView = findViewById(R.id.token_text_view);
+        final TextView tokenView = (TextView) findViewById(R.id.token_text_view);
         tokenView.setText(getString(R.string.token, mAccessToken));
-    }
-
-    private void updateCodeView() {
-        final TextView codeView = findViewById(R.id.code_text_view);
-        codeView.setText(getString(R.string.code, mAccessCode));
     }
 
     private void cancelCall() {
@@ -150,4 +126,48 @@ public class MainActivity extends AppCompatActivity {
                 .authority(getString(R.string.com_spotify_sdk_redirect_host))
                 .build();
     }
+
+    public void authenticationButtonClicked(View view) {
+
+        // request access token
+        requestTokenRequested();
+
+        // if access token has not been received, show snackbar message
+        if (mAccessToken == null) {
+            final Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_main), R.string.warning_need_token, Snackbar.LENGTH_SHORT);
+            snackbar.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+            snackbar.show();
+            return;
+        }
+
+        // build the request object using the access token
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me")
+                .addHeader("Authorization","Bearer " + mAccessToken)
+                .build();
+
+        // cancel the call if there is none
+        cancelCall();
+        mCall = mOkHttpClient.newCall(request);
+
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                setResponse("Failed to fetch data: " + e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    // create JSON response body for user details
+                    final JSONObject jsonObject = new JSONObject(response.body().string());
+                    setResponse(jsonObject.toString(3));
+                }
+                catch (JSONException e) {
+                    // throw exception
+                    setResponse("Failed to parse data: " + e);
+                }
+            }
+        });
+    }
 }
+
