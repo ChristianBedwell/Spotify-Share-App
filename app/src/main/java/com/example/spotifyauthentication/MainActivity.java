840859d333da64b68fc8ca0914dced0a87a1ae86
Christian Bedwell <Christian.8edwell@gmail.com>
2019-07-07 11:39:03 -0400

    Minor UI changes to MainActivity for user convenience, access token is now passed as an intent extra to second activity.

--- app/src/main/java/com/example/spotifyauthentication/MainActivity.java
+++ app/src/main/java/com/example/spotifyauthentication/MainActivity.java
@@ -10,35 +10,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.Locale;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    // constants
     public static final String CLIENT_ID = "clientid";
     public static final int AUTH_TOKEN_REQUEST_CODE = 0x10;
+    public static final String EXTRA_MESSAGE = "com.example.spotifyauthentication.extra.MESSAGE";
 
-    private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken;
-    private Call mCall;
 
     CheckBox checkbox;
-    private Button button_authenticate;
+    Button buttonAuthenticate;
+    ProgressBar progressBar;
+    TextView progressMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,35 +40,49 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(String.format(
                 Locale.US, "Spotify Analytics", com.spotify.sdk.android.authentication.BuildConfig.VERSION_NAME));
 
-        // instantiate agreement checkbox and authentication button
+        // instantiate agreement checkbox, authentication button, and status bar/message
         checkbox = (CheckBox) findViewById(R.id.checkbox);
-        button_authenticate = (Button) findViewById(R.id.authentication_button);
+        buttonAuthenticate = (Button) findViewById(R.id.authentication_button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressMessage = (TextView) findViewById(R.id.progressMessage);
 
         // by default, checkbox state is off because the user hasn't clicked the checkbox
-        button_authenticate.setEnabled(false);
+        buttonAuthenticate.setEnabled(false);
 
+        // by default, progress bar/message is invisible because the user hasn't clicked authenticate
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+        progressMessage.setVisibility(TextView.INVISIBLE);
+
+        // create click listener for checkbox state changes
         checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             // checks checkbox state, used to toggle authentication button state on and off
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // if user has agreed to authentication statement, enable authentication button
                 if (isChecked) {
-                    button_authenticate.setEnabled(true);
+                    buttonAuthenticate.setEnabled(true);
                 }
                 // if user has not agreed to authentication statement, disable authentication button
-                else{
-                    button_authenticate.setEnabled(false);
+                else {
+                    buttonAuthenticate.setEnabled(false);
                 }
+            }
+        });
+
+        // create click listener for authentication button clicks
+        buttonAuthenticate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // request access token
+                requestToken();
+
+                // run a background task in webview, display progress bar and progress message
+                progressBar.setVisibility(ProgressBar.VISIBLE);
+                progressMessage.setVisibility(TextView.VISIBLE);
             }
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        cancelCall();
-        super.onDestroy();
-    }
-
-    public void requestTokenRequested() {
+    public void requestToken() {
         final AuthenticationRequest request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN);
         AuthenticationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request);
     }
@@ -93,31 +100,22 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
 
+        // if access token is not received, show snackbar message
+        if (mAccessToken == null) {
+            final Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_main), R.string.warning_need_token, Snackbar.LENGTH_SHORT);
+            snackbar.getView().setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+            snackbar.show();
+        }
+
+        // check response for equivalent access token
         if (AUTH_TOKEN_REQUEST_CODE == requestCode) {
             mAccessToken = response.getAccessToken();
-            updateTokenView();
         }
-    }
 
-    private void setResponse(final String text) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                final TextView responseView = findViewById(R.id.response_text_view);
-                responseView.setText(text);
-            }
-        });
-    }
-
-    private void updateTokenView() {
-        final TextView tokenView = (TextView) findViewById(R.id.token_text_view);
-        tokenView.setText(getString(R.string.token, mAccessToken));
-    }
-
-    private void cancelCall() {
-        if (mCall != null) {
-            mCall.cancel();
-        }
+        // store access token in intent extra and pass it to MostPopularActivity
+        Intent intent = new Intent(getApplicationContext(), MostPopularActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mAccessToken);
+        startActivity(intent);
     }
 
     private Uri getRedirectUri() {
@@ -125,49 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
                 .authority(getString(R.string.com_spotify_sdk_redirect_host))
                 .build();
-    }
-
-    public void authenticationButtonClicked(View view) {
-
-        // request access token
-        requestTokenRequested();
-
-        // if access token has not been received, show snackbar message
-        if (mAccessToken == null) {
-            final Snackbar snackbar = Snackbar.make(findViewById(R.id.activity_main), R.string.warning_need_token, Snackbar.LENGTH_SHORT);
-            snackbar.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
-            snackbar.show();
-            return;
-        }
-
-        // build the request object using the access token
-        final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me")
-                .addHeader("Authorization","Bearer " + mAccessToken)
-                .build();
-
-        // cancel the call if there is none
-        cancelCall();
-        mCall = mOkHttpClient.newCall(request);
-
-        mCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                setResponse("Failed to fetch data: " + e);
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    // create JSON response body for user details
-                    final JSONObject jsonObject = new JSONObject(response.body().string());
-                    setResponse(jsonObject.toString(3));
-                }
-                catch (JSONException e) {
-                    // throw exception
-                    setResponse("Failed to parse data: " + e);
-                }
-            }
-        });
     }
 }
 
--- app/src/main/java/com/example/spotifyauthentication/MostPopularActivity.java
+++ app/src/main/java/com/example/spotifyauthentication/MostPopularActivity.java
@@ -1,5 +1,83 @@
 package com.example.spotifyauthentication;
 
-public class MostPopularActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MostPopularActivity extends AppCompatActivity {
+
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    private String mAccessToken;
+    private Call mCall;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_most_popular);
+
+        // Retrieve access token from intent extra and store it
+        Intent intent = getIntent();
+        mAccessToken = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    }
+
+    private void cancelCall() {
+        if (mCall != null) {
+            mCall.cancel();
+        }
+    }
+
+    private void setResponse(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final TextView responseView = findViewById(R.id.response_text_view);
+                responseView.setText(text);
+            }
+        });
+    }
+
+    public void buildRequest() {
+        // build the request object using the access token
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me")
+                .addHeader("Authorization","Bearer " + mAccessToken)
+                .build();
+
+        // cancel the call if there is none
+        cancelCall();
+        mCall = mOkHttpClient.newCall(request);
+
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                setResponse("Failed to fetch data: " + e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    // create JSON response body for user details
+                    final JSONObject jsonObject = new JSONObject(response.body().string());
+                    setResponse(jsonObject.toString(3));
+                }
+                catch (JSONException e) {
+                    // throw exception
+                    setResponse("Failed to parse data: " + e);
+                }
+            }
+        });
+    }
 
 }
